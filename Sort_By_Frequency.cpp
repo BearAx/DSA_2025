d7bea3b83ba3bd3a571a1446ecf887ab039bdbf9
BearAx <klorik900@gmail.com>
2025-04-26 12:14:09 +0300

    Sort By Frequency
    Given N integers, sort the numbers in the ascending order based on their appearance frequency in the input. If frequencies tie, you can print the numbers in any arbitrary order.

--- Sort_By_Frequency.cpp
+++ Sort_By_Frequency.cpp
@@ -1,86 +1,46 @@
-#include <iostream>
-#include <vector>
+#include <cstdio>
 #include <unordered_map>
+#include <vector>
+#include <cstdlib>
 
-using namespace std;
+std::unordered_map<int, int> global_freq;
 
-bool customCompare(const pair<int, pair<int, int>>& x, const pair<int, pair<int, int>>& y) {
-	if (x.second.first != y.second.first) {
-		return x.second.first < y.second.first;
-	}
-	if (x.first != y.first) {
-		return x.first < y.first;
-	}
-	return x.second.second < y.second.second;
-}
-
-void mergeVectors(vector<pair<int, pair<int, int>>>& data, int start, int mid, int end) {
-	int sizeLeft = mid - start + 1;
-	int sizeRight = end - mid;
-	vector<pair<int, pair<int, int>>> leftPart(sizeLeft);
-	vector<pair<int, pair<int, int>>> rightPart(sizeRight);
-
-	for (int i = 0; i < sizeLeft; ++i) {
-		leftPart[i] = data[start + i];
-	}
-	for (int j = 0; j < sizeRight; ++j) {
-		rightPart[j] = data[mid + 1 + j];
-	}
-
-	int i = 0, j = 0, k = start;
-
-	while (i < sizeLeft && j < sizeRight) {
-		if (customCompare(leftPart[i], rightPart[j])) {
-			data[k++] = leftPart[i++];
-		}
-		else {
-			data[k++] = rightPart[j++];
-		}
-	}
-
-	while (i < sizeLeft) {
-		data[k++] = leftPart[i++];
-	}
-
-	while (j < sizeRight) {
-		data[k++] = rightPart[j++];
-	}
-}
-
-void performMergeSort(vector<pair<int, pair<int, int>>>& data, int start, int end) {
-	if (start >= end) {
-		return;
-	}
-	int middle = start + (end - start) / 2;
-	performMergeSort(data, start, middle);
-	performMergeSort(data, middle + 1, end);
-	mergeVectors(data, start, middle, end);
+int compare(const void *a, const void *b) {
+    int val1 = *(int*)a;
+    int val2 = *(int*)b;
+    int freq1 = global_freq[val1];
+    int freq2 = global_freq[val2];
+    if (freq1 != freq2) {
+        return freq1 - freq2;
+    } else {
+        return val1 - val2;
+    }
 }
 
 int main() {
-	ios::sync_with_stdio(false);
-	cin.tie(nullptr);
+    int n;
+    scanf("%d", &n);
+    std::vector<int> nums(n);
+    for (int i = 0; i < n; ++i) {
+        scanf("%d", &nums[i]);
+    }
 
-	int totalElements;
-	cin >> totalElements;
-	vector<int> inputArray(totalElements);
-	unordered_map<int, int> elementFrequency;
+    std::unordered_map<int, int> freq;
+    for (int num : nums) {
+        freq[num]++;
+    }
 
-	for (int i = 0; i < totalElements; ++i) {
-		cin >> inputArray[i];
-		elementFrequency[inputArray[i]]++;
-	}
+    std::vector<int> unique;
+    for (const auto& pair : freq) {
+        unique.push_back(pair.first);
+    }
 
-	vector<pair<int, pair<int, int>>> indexedData;
-	for (int i = 0; i < totalElements; ++i) {
-		indexedData.push_back({ inputArray[i], {elementFrequency[inputArray[i]], i} });
-	}
+    global_freq = freq;
+    qsort(unique.data(), unique.size(), sizeof(int), compare);
 
-	performMergeSort(indexedData, 0, indexedData.size() - 1);
+    for (int num : unique) {
+        printf("%d\n", num);
+    }
 
-	for (const auto& entry : indexedData) {
-		cout << entry.first << " " << entry.second.second << "\n";
-	}
-
-	return 0;
+    return 0;
 }
